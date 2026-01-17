a89c0e9b99502dd16b6a83522d8b686109f16b05
peter <peter.berghuis_13@hotmail.com>
2026-01-17 19:43:41 +0900

    git init

---
+++ src/main/java/io/github/peterberghuis/projecttanukiback/ProjectTanukiBackApplication.java
@@ -0,0 +1,13 @@
+package io.github.peterberghuis.projecttanukiback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTanukiBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectTanukiBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/peterberghuis/projecttanukiback/ProjectTanukiBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.peterberghuis.projecttanukiback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTanukiBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
